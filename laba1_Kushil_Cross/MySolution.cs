9cd9a1f61da3b3c62e6ea16face3c38abb16b678
Akemax <silver.maks10@gmail.com>
2021-06-04 02:35:33 +0300

    Добавьте файлы проекта.

---
+++ laba1_Kushil_Cross/MySolution.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MyLib
+{
+    public class MySolution
+    {
+        public static double fac(double i)
+        {
+            if (i == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return i * fac(i - 1);
+            }
+        }
+        public static double sum_row(double x, double n)
+        {
+            double res = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                res = res + Math.Pow(x, i * 2) / fac(i * 2);
+            }
+            return res;
+            
+
+        }
+        public static double func_ch(double x)
+        {
+            double ch;
+            ch = (Math.Pow(Math.E, x) + Math.Pow(Math.E, (-x))) / 2;
+            return ch;
+
+        }
+        public static double Perimeter(double side)
+        {
+            double per;
+            per = 4 * side;
+            return per;
+        }
+        public static double Sum(double x, double n)
+        {
+            double S = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i == 0)
+                {
+                    S = 1;
+                }
+                S = S + (Math.Pow(x * Math.Log(x, Math.E), i) / fac(i));
+            }
+            return S;
+        }
+        public static double Func_t(double x, double n)
+        {
+            double t;
+            t = Math.Abs(sum_row(x, n) - func_ch(x)) / func_ch(x) * 100;
+            return t;
+        }
+        
+      
+    }
+}
---
+++ laba1_Kushil_Cross/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace laba1_Kushil_Cross
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вариант №4");
+            Console.WriteLine("Tusk #1");
+            Console.Write("Введите х - длину стороны квадрата: ");
+            double x1 = double.Parse(Console.ReadLine());
+            double Tusk1 = MyLib.MySolution.Perimeter(x1);
+            Console.WriteLine("Периметр квадрата равен {0}", Tusk1);
+
+            Console.WriteLine("Tusk #2");
+            Console.Write("Введите х - действительное число: ");
+            double x2 = double.Parse(Console.ReadLine());
+            Console.Write("Введите n - натуральное число: ");
+            double n2 = double.Parse(Console.ReadLine());
+            double Tusk2 = MyLib.MySolution.Sum(x2, n2);
+            Console.WriteLine(Tusk2.ToString());
+
+            Console.WriteLine("Tusk #3");
+            Console.Write("Введите х - действительное число: ");
+            double x3 = double.Parse(Console.ReadLine());
+            Console.Write("Введите n - кол-во членов в ряде: ");
+            double n3 = double.Parse(Console.ReadLine());
+            double Tusk3 = MyLib.MySolution.Func_t(x3, n3);
+            Console.WriteLine(Tusk3.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
